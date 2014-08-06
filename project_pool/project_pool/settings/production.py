9832e5a53fd993f28e9008920a9fa0f792384d08
Suleyman Melikoglu <suleymanmelikoglu@gmail.com>
2014-08-06 11:28:03 +0300

    production settings

--- project_pool/project_pool/settings/production.py
+++ project_pool/project_pool/settings/production.py
@@ -7,12 +7,19 @@ TEMPLATE_DEBUG = False
 INSTALLED_APPS += (
 )
 
+ALLOWED_HOSTS = ["*"]  # replace it with yourhost.com
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'db_name',
-        'USER': 'DB_USER',
-        'PASSWORD': 'DB_PASSWORD',
+
+        'NAME': 'project_pool',
+        'USER': 'root',
+        'PASSWORD': '',
+
+        #'NAME': 'db_name',
+        #'USER': 'DB_USER',
+        #'PASSWORD': 'DB_PASSWORD',
         'HOST': 'localhost',   # Or an IP Address that your DB is hosted on
         'PORT': '3306',
     }
